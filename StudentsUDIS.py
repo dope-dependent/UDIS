a0948af03989db30db859adf067a7b0b347b13be
pasthorizon <seemanthachari@gmail.com>
2021-03-05 09:36:06 +0530

    Formatted the StudentUDIS page

--- StudentsUDIS.py
+++ StudentsUDIS.py
@@ -18,36 +18,37 @@ class StudentMainMenu:
         self.frame = Frame(root,bg="red")
         self.frame.grid(row=0, column=0, sticky='nsew')
 
-        self.add_button_StudentMainMenu = Button(self.frame, text="Add New Student",
+        self.add_button_StudentMainMenu = Button(self.frame, text="Add New Student",anchor=W,
                                                  command = self.add_command_StudentMainMenu)
 
-        self.reg_button_StudentMainMenu = Button(self.frame, text="Register Courses",
+        self.reg_button_StudentMainMenu = Button(self.frame, text="Register Courses",anchor=W,
                                                  command=self.add_command_StudentMainMenu)
 
-        self.view_button_StudentMainMenu = Button(self.frame, text="View Student",
+        self.view_button_StudentMainMenu = Button(self.frame, text="View Student",anchor=W,
                                                  command=self.add_command_StudentMainMenu)
 
-        self.grade_button_StudentMainMenu = Button(self.frame, text="Enter Student Grades",
+        self.grade_button_StudentMainMenu = Button(self.frame, text="Enter Student Grades",anchor=W,
                                                  command=self.add_command_StudentMainMenu)
 
         self.exit_button_StudentMainMenu = Button(self.frame, text="Exit", command=exit)
         self.back_button_StudentMainMenu = Button(self.frame, text="Back", command=lambda:self.back_command_StudentMainMenu(root))
 
-        self.add_button_StudentMainMenu.grid(row=0, column=0, columnspan=2)
-        self.reg_button_StudentMainMenu.grid(row=1, column=0, columnspan=2)
-        self.view_button_StudentMainMenu.grid(row=2, column=0, columnspan=2)
-        self.grade_button_StudentMainMenu.grid(row=3, column=0, columnspan=2)
+        self.add_button_StudentMainMenu.grid(row=0, column=0, columnspan=2,sticky=E+W)
+        self.reg_button_StudentMainMenu.grid(row=1, column=0, columnspan=2,sticky=E+W)
+        self.view_button_StudentMainMenu.grid(row=2, column=0, columnspan=2,sticky=E+W)
+        self.grade_button_StudentMainMenu.grid(row=3, column=0, columnspan=2,sticky=E+W)
 
-        self.exit_button_StudentMainMenu.grid(row=4, column=0)
-        self.back_button_StudentMainMenu.grid(row=4, column=1)
+        self.exit_button_StudentMainMenu.grid(row=4, column=0,pady=10,sticky=S)
+        self.back_button_StudentMainMenu.grid(row=4, column=1,pady=10,sticky=S)
 
         self.frame.columnconfigure(0, weight=1)
         self.frame.columnconfigure(1, weight=1)
 
-        self.frame.rowconfigure(0, weight=1)
-        self.frame.rowconfigure(1, weight=1)
-        self.frame.rowconfigure(2, weight=1)
-        self.frame.rowconfigure(3, weight=1)
+        self.frame.rowconfigure(0, weight=0)
+        self.frame.rowconfigure(1, weight=0)
+        self.frame.rowconfigure(2, weight=0)
+        self.frame.rowconfigure(3, weight=0)
+        self.frame.rowconfigure(4, weight=1)
 
         # root.mainloop()
 
