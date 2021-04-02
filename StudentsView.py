f85a4c50d9ffb71c606e7acebee77d543e711acf
pasthorizon <seemanthachari@gmail.com>
2021-04-02 10:58:52 +0530

    Performance for ongoing courses handled

--- StudentsView.py
+++ StudentsView.py
@@ -21,7 +21,7 @@ class StudentsView (StudentSearch.StudentSearch):
         roll = roll[0]
         cursor_.execute('SELECT * FROM student WHERE roll=(:roll)', {'roll':roll})
         student = cursor_.fetchone()
-        cursor_.execute('SELECT * FROM courses_taken WHERE roll=(:roll)',{'roll':roll})
+        cursor_.execute('SELECT * FROM courses_taken WHERE roll=(:roll) AND grade!=(:grade)',{'roll':roll,'grade':'R'})
         allCourses=cursor_.fetchall()
 
         Label(popupStudent, text='Roll No.').grid(row=1, column=0, sticky=W, padx=10)
@@ -49,7 +49,7 @@ class StudentsView (StudentSearch.StudentSearch):
 
     def fetchPerformance(self,roll_no):
         connect_,cursor_=ES.get_student_db_ES()
-        cursor_.execute('SELECT * FROM courses_taken WHERE roll=(:roll)',{'roll':roll_no})
+        cursor_.execute('SELECT * FROM courses_taken WHERE roll=(:roll) AND grade!=(:grade)',{'roll':roll_no,'grade':'R'})
         allCourses=cursor_.fetchall()
         semDict={}
         for i in allCourses:
@@ -112,10 +112,12 @@ class StudentsView (StudentSearch.StudentSearch):
         backlogsFrame.columnconfigure(3,weight=1)
 
 
-        ind=0
+        ind=2
         for i in semDict:
-            self.semPerformance(perfView,2+ind,i,semDict[i])
+            self.semPerformance(perfView,ind,i,semDict[i])
             ind=ind+1
+
+        self.ongoingSemester(student[0],perfView,ind)
 
         perfView.frame.columnconfigure(0,weight=1)
         
@@ -148,7 +150,31 @@ class StudentsView (StudentSearch.StudentSearch):
 
         semFrame.columnconfigure(1,weight=1)
 
+    def ongoingSemester(self,roll_no,perfView,row_):
+        connect_,cursor_=ES.get_student_db_ES()
+        cursor_.execute('SELECT sub_code,sem_taken FROM courses_taken WHERE roll=(:roll) AND grade=(:grade)',{'roll':roll_no,'grade':'R'})
+        courselist=cursor_.fetchall()
+
+        ongoingFrame=Frame(perfView.frame,bg='red')
+        ongoingFrame.grid(column=0,row=row_,sticky=E+W,pady=10)
+        Label(ongoingFrame,text='Ongoing Semester: '+courselist[0][1],relief=GROOVE,anchor=W).grid(row=0,column=0,columnspan=3,sticky=E+W)        
+        Label(ongoingFrame,text='Course Code',relief=GROOVE,width=12).grid(row=1,column=0,sticky=E+W)
+        Label(ongoingFrame,text='Course Name',relief=GROOVE).grid(row=1,column=1,sticky=E+W)
+        Label(ongoingFrame,text='Credits',width=12,relief=GROOVE).grid(row=1,column=2,sticky=E+W)
+
+        ind=0
+        for i in courselist:
+            cursor_.execute('SELECT course_name,credits FROM all_courses WHERE sub_code=(:code)',{'code':i[0]})
+            info=cursor_.fetchone()
+            Label(ongoingFrame,text=i[0]).grid(row=2+ind,column=0,sticky=E+W)
+            Label(ongoingFrame,text=info[0]).grid(row=2+ind,column=1,sticky=E+W)
+            Label(ongoingFrame,text=info[1]).grid(row=2+ind,column=2,sticky=E+W)
+            ind=ind+1
+
+        ongoingFrame.columnconfigure(1,weight=1)
+
     def getCGPA(self,allCourses):
+        
         connect_,cursor_=ES.get_student_db_ES()
         gradesDict={'Ex':10,'A':9,'B':8,'C':7,'D':6,'P':5,'F':0}
         
